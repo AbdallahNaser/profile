5a76687a4521c317162aecc76684b33e96e720a3
unknown <abdallahnser555@gmail.com>
2025-10-25 00:44:49 +0300

    update file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/my-portfolio/',  // <-- important for project pages
+  base: '/my-portfolio/',  // important for GitHub Pages
   plugins: [react()],
 });
